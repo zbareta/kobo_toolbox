7d2d7ef3930221b2d5014bf6bc80e09fdff2e542
Željko Bareta <zbareta@gmail.com>
2017-08-20 21:42:51 +0200

    Create curl.php
    Connect to kobo.unhcr.org dataset through API

---
+++ curl.php
@@ -0,0 +1,18 @@
+<?php
+$username = "user";
+$password = "pass";
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($curl, CURLOPT_URL, 'https://kobocat.unhcr.org/bareta/forms/a34q37aM3i2GDTcdTs6oWv/api');
+curl_setopt($curl, CURLOPT_USERPWD, "$username:$password");
+curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+$resp = curl_exec($curl);
+$array = json_decode($resp,true);
+if($errno = curl_errno($curl)) {
+$error_message = curl_strerror($errno);
+echo "Error ({$errno}):\n {$error_message}";
+}
+curl_close($curl); 
+?>
